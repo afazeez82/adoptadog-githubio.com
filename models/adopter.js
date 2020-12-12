df19d0748e18d578a923c1db51a42633bfa99100
afazeez <afazeez82@gmail.com>
2020-12-12 12:06:13 -0600

    deleted dogs.js file not needed

--- models/adopter.js
+++ models/adopter.js
@@ -1,13 +1,11 @@
 
 module.exports = function (sequelize, DataTypes)  {
-  const adopter = sequelize.define("Adopter", {
-<<<<<<< Updated upstream
-=======
+  const adopter = sequelize.define("adopter", {
+
     user: {
       type: sequelize.Schema.Types.ObjectId,
       ref: 'user'
     },
->>>>>>> Stashed changes
     contactName: {
       type: DataTypes.STRING,
       allowNull: false
