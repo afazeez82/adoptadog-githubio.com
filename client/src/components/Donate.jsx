ef5eb083cfa59cc90bf07a4fc109010da9ee32ef
Caroline Pukenis <caro_pukenis@yahoo.com>
2020-12-12 13:16:33 -0600

    donatePage

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -2,27 +2,40 @@ import React from "react";
 
 function Donate() {
     return (
-        <div className="Donate">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <img
-                            class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""
-                        />
-                    </div>
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Donate</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
+        <div className="Donate" style={{
+            backgroundImage: "url('https://pethome.hr/upload/2020/09/dogs-need-to-play_5f56183e18f4f.jpg')", height: "1200px", maxWidth: "1900px", minWidth: "1500px"
+        }}>
+            <div className="container">
+                <div className="row p-5">
+                    <div className="col-10 offset-lg-7">
+                        <div className="p-4 col-8" style={{ backgroundColor: "#F04862" }}>
+                            <div className="p-2" style={{ backgroundColor: "#F48B7D" }}>
+                                <div class="sr-root">
+                                    <div class="sr-main">
+                                        <header class="sr-header">
+                                            <div class="sr-header__logo"></div>
+                                        </header>
+                                        <div class="sr-payment-summary payment-view">
+                                            <h1 class="order-amount">$14.00</h1>
+                                            <h4>per month subscription</h4>
+                                        </div>
+                                        <div class="sr-payment-form payment-view">
+                                            <label class="sr-checkbox-label"></label>
+                                            <span>Add optional donation</span>
+                                            <div class="donations">
+                                                <button class="donation" data-amount="1">$1</button>
+                                                <button class="donation" data-amount="5">$5</button>
+                                                <button class="donation" data-amount="20">$20</button>
+                                            </div>
+                                            <button id="submit">Checkout</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </div>
     );
 }
