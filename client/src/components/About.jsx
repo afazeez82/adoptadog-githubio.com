4191643a2dd5a83305506aeaa947c189e4bb62e3
“Nasra <nasra_09@yahoo.com>
2020-12-12 18:17:06 +0300

    About and Landing pages commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import "../index.css"
 
 function About() {
   return (
     <div className="about">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container">
+        <div className="row align-items-center my-5">
+           <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
+              className="img-fluid w-100 h-auto"
+              src={process.env.PUBLIC_URL +"/images/white-dog.jpg"}
+              alt="White Dog"
             />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
+          </div> 
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">About</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
+            <div className="col-md-4"></div>
+            <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#donate">DONATE</a>
+            <img src={process.env.PUBLIC_URL +"/images/cute-dog.jpeg"} className="img-circle" alt="..."></img>
+            <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#blog">BLOG</a>
+            <img src={process.env.PUBLIC_URL +"/images/dog2.jpg"} className="img-circle" alt="..."></img>
           </div>
         </div>
       </div>
