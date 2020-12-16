39ec1c9aa1492b0bd76670ffa4fc5168cc86dacb
caropukenis <56455360+caropukenis@users.noreply.github.com>
2020-12-16 00:30:24 -0600

    Revert "link buttons"
    This reverts commit a3241051e05801ec3372d74adb671d5b01af742d.

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div className="container">
+    <div className="about">
       <div className="row">
         <img
-          className="w-100"
+          className="img-fluid w-100 h-auto"
           src={process.env.PUBLIC_URL + "/images/dogs1.jpg"}
           alt="White Dog"
         />
       </div>
-      <div className="row " style={{ backgroundColor: "#004179", height: "400px" }}>
-        <div className="col-6 m-3" style={{ backgroundColor: "#5EBAF2", height: "350px" }}>
-          <h5 className="p-3">
+      <div className="row justify-content-md-center" style={{ backgroundColor: "#004179", height: "400px" }}>
+        <div className="col-5 m-3" style={{ backgroundColor: "#5EBAF2", height: "350px" }}>
+          <h5 className="p-4">
             FurEver Home Rescue is a 501(c)(3) nonprofit based in
             Minneasota and focuses on dogs who need a new home.
 
             At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
             </h5>
         </div>
-        <div className="col-5 m-2 row">
-        <Link to="/donate">
-          <p style={{ color: "#CACACA" }}>DONATE</p>
-          <img src={process.env.PUBLIC_URL + "/images/cute-dog.jpeg"} className="img-circle m-4" alt="..." />
-        </Link>
-
-        <Link to="/blog">
-          <p style={{ color: "#CACACA" }}>BLOG</p>
-          <img src={process.env.PUBLIC_URL + "/images/dog3.jpg"} className="img-circle m-4" alt="..." />
-        </Link>
+        <div className="col-6">
+          <button className="btn-primary p-3">
+            <p style={{ color: "#CACACA" }}>DONATE</p>
+            <img src={process.env.PUBLIC_URL + "/images/dog4.jpg"} className="img-circle m-4" alt="..." />
+          </button>
+          <button className="btn-primary p-3">
+            <p style={{ color: "#CACACA" }}>BLOG</p>
+            <img src={process.env.PUBLIC_URL + "/images/dog3.jpg"} className="img-circle m-4" alt="..." />
+          </button>
         </div>
       </div>
     </div >
--- client/src/views/Footer.jsx
+++ client/src/views/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer className="p-3 bg-light">
+      <footer className="py-5 bg-dark fixed-bottom">
         <div className="container">
-          <p className="m-0 text-center">
-            FurEver Home Rescue Copyright 2020
+          <p className="m-0 text-center text-white">
+            Copyright 2020
           </p>
         </div>
       </footer>
