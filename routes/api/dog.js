5e460248d5bbca1346ef0bbe49c38f42da8d98f1
afazeez <afazeez82@gmail.com>
2020-12-16 21:38:44 -0600

    updated the filter api

--- routes/api/dog.js
+++ routes/api/dog.js
@@ -1,5 +1,6 @@
 
 const router = require("express").Router();
+//const app = require('express') ();
 //const dogController = require("dogController");
 
 
@@ -10,72 +11,71 @@ module.exports = function (router) {
     // Create a new Dog
     router.post("/api/dog", (req, res) => {
         db.dog.create({
-                dogName: req.body.dogName,
-                dogImage: req.body.dogImage,
-                dogAge: req.body.dogAge,
-                gender: req.body.gender,
-                dogBreed: req.body.dogBreed,
-                coatLength: req.body.coatLength,
-                activityLevel: req.body.activityLevel,
-                specialNeeds: req.body.specialNeeds,
-                briefBio: req.body.briefBio
-            })
-            .then(function(dog) {
+            dogName: req.body.dogName,
+            dogImage: req.body.dogImage,
+            dogAge: req.body.dogAge,
+            gender: req.body.gender,
+            dogBreed: req.body.dogBreed,
+            coatLength: req.body.coatLength,
+            activityLevel: req.body.activityLevel,
+            specialNeeds: req.body.specialNeeds,
+            briefBio: req.body.briefBio
+        })
+            .then(function (dog) {
                 res.json(dog);
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 res.status(500).json(err);
             });
     });
 
-     // Retrieve all Dog
-     router.get("/api/dog", (req, res) => {
-        db.dog.findAll({}).then(function(dbGetDog) {
-                res.json(dbGetDog);
-            })
-            .catch(function(err) {
-                res.status(500).json(err);
-            });
-    });
-    // Retrieve all Dogs based on filter
-    router.get("/api/dog/:dogBreed/:dogAge/:gender/:coatLength/:activityLevel/:specialNeeds", function(req, res) {
-        db.dog
-            .findAll({
-                where: {
-                    dogBreed: req.body.dogBreed,
-                    dogAge: req.body.dogAge,
-                    gender: req.body.gender,
-                    coatLength: req.body.coatLength,
-                    activityLevel: req.body.activityLevel,
-                    specialNeeds: req.body.specialNeeds
-                },
-            })
-            .then(function(dbGetDog) {
-                res.json(dbGetDog);
-            })
-            .catch(function(err) {
+    // Retrieve all Dog
+    router.get("/api/dog", (req, res) => {
+        db.dog.findAll({}).then(function (dbGetDog) {
+            res.json(dbGetDog);
+        })
+            .catch(function (err) {
                 res.status(500).json(err);
             });
     });
 
-    // router.get('/api/dog/:dogBreed/:dogAge/:gender/:coatLength/:activityLevel/:specialNeeds', function(req, res) {
-    //     getFilterBasedValues(req.body.dogBreed, req.body.dogAge, req.body.gender, req.body.coatLength, req.body.activityLevel, req.body.specialNeeds);
-    //   });
+    // filter api
+    router.get('/dog', function (req, res) {
+        console.log(req.query);
+        db.dog.findAll({
+
+            where: {
+                dogBreed: req.query.dogBreed,
+                dogAge: req.query.dogAge,
+                gender: req.query.gender,
+                coatLength: req.query.coatLength,
+                activityLevel: req.query.activityLevel,
+                specialNeeds: req.query.specialNeeds
+            }
+
+
+        }).then(function (resp) {
+            res.json(resp);
+        })
+
+    });
 
 
 
 
-     // Retrieve a single Dog by Id
 
-     router.get("/api/dog/:id", (req, res) => {
+
+    // Retrieve a single Dog by Id
+
+    router.get("/api/dog/:id", (req, res) => {
         db.dog.findOne({
-                where: {
-                    id: req.params.id
-                }
-            }).then(function(dbGetDog) {
-                res.json(dbGetDog );
-            })
-            .catch(function(err) {
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbGetDog) {
+            res.json(dbGetDog);
+        })
+            .catch(function (err) {
                 res.status(500).json(err);
             });
     });
@@ -83,39 +83,39 @@ module.exports = function (router) {
     // Delete a Dog with Id
     router.delete("/api/dog/:id", (req, res) => {
         db.dog.destroy({
-                where: {
-                    id: req.params.id
-                }
-            })
-            .then(function(dbDeleteDog) {
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (dbDeleteDog) {
                 res.json(dbDeleteDog);
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 res.status(500).json(err);
             });
     });
 
-      // Update a Dog with Id
-      router.put("/api/dog/:id", (req, res) => {
+    // Update a Dog with Id
+    router.put("/api/dog/:id", (req, res) => {
         db.dog.update({
-                dogName: req.body.dogName,
-                dogImage: req.body.dogImage,
-                dogAge: req.body.dogAge,
-                gender: req.body.gender,
-                dogBreed: req.body.dogBreed,
-                coatLength: req.body.coatLength,
-                activityLevel: req.body.activityLevel,
-                specialNeeds: req.body.specialNeeds,
-                briefBio: req.body.briefBio,
-                updatedAt: Date.now(),
-            }, {
-                where: {
-                    id: req.params.id
-                }
-            }).then(function(dbUpdateDog) {
-                res.json(dbUpdateDog);
-            })
-            .catch(function(err) {
+            dogName: req.body.dogName,
+            dogImage: req.body.dogImage,
+            dogAge: req.body.dogAge,
+            gender: req.body.gender,
+            dogBreed: req.body.dogBreed,
+            coatLength: req.body.coatLength,
+            activityLevel: req.body.activityLevel,
+            specialNeeds: req.body.specialNeeds,
+            briefBio: req.body.briefBio,
+            updatedAt: Date.now(),
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbUpdateDog) {
+            res.json(dbUpdateDog);
+        })
+            .catch(function (err) {
                 res.status(500).json(err);
             });
     });
